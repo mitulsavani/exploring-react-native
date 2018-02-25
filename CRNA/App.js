1e60e806ec2c4fd1bd0f436fbf159b14adf27a70
mitulsavani <mitul.savani2@yahoo.com>
2018-02-25 09:01:13 -0800

    Revert "Done with the input component, now working on FlatList"
    This reverts commit 07a4474f067ce8835294b4ef78abfd66092cf1df.

--- CRNA/App.js
+++ CRNA/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import {  StyleSheet, Text, View, ScrollView, Image, Button, Alert, TextInput, Keyboard } from 'react-native';
+import {  StyleSheet, Text, View, ScrollView, Image, Button, Alert } from 'react-native';
 
 
 var backgroundcolors = ['green', 'wheat', 'orange', 'blue', 'purple', 'pink', 'yellow', 'aqua'];
-var backgroundcolors1 = ['green', 'wheat', 'orange', 'blue', 'purple', 'pink', 'yellow', 'aqua'];
 
 
 export default class App extends React.Component {
@@ -11,48 +10,18 @@ export default class App extends React.Component {
     super(props);
   
     this.state = {
-      backgroundColor: '#B67086',
-      text: '',
+      backgroundColor: 'red',
     };
   }
 
   changeBackground = () => {
     var backgroundColor = backgroundcolors[Math.floor(Math.random()*backgroundcolors.length)];
-
-    this.setState({
-        backgroundColor: backgroundColor,
+      this.setState({
+        backgroundColor: backgroundColor
       });
   }
 
-  handleSubmit = () => 
-  {
-    const { text } = this.state
-    
-    if (this.state.text === 'Passionate') {
-      Keyboard.dismiss
-      Alert.alert(
-        'Success',
-        'Lets play a festival of colors; Holi',
-        [
-          { text: 'OK', onPress: () => console.log('OK Pressed') },
-        ],
-        { cancelable: false })
-    } 
-    else {
-      Keyboard.dismiss
-      Alert.alert(
-        'Invalid',
-        'Ima PASSIONATE tutor',
-        [
-          { text: 'Try again', onPress: () => console.log('Try again pressed') }
-        ],
-        { cancelable: false }
-      )
-    }
-  }
-
   render() {
-    
     return (
       <ScrollView style={styles.container}>
         <View style={styles.view1}>
@@ -61,35 +30,31 @@ export default class App extends React.Component {
           <Image style={styles.mainimage} source = {require('./Holi.jpg')} />
         </View>
         <View style={styles.view3}>
-          <Image style={styles.subimage} source = {require('./handholi.jpg')} />
-          <Image style={styles.subimage} source = {require('./holyface1.jpg')} />
+          <Image style={styles.subimage} source = {require('./buddha.jpg')} />
+          <Image style={styles.subimage} source = {require('./hanuman.png')} />
         </View>
-        <View style={[styles.view4, {backgroundColor: this.state.backgroundColor}]} >
+        <View style={{
+          //flex: 4,
+          backgroundColor: this.state.backgroundColor,
+          height: 400,
+          justifyContent: 'center',
+          alignItems: 'center' }}>
           <View style={{
-              backgroundColor: '#EFB599',
+              backgroundColor: 'red',
               padding: 10,
               borderRadius: 16,
               shadowRadius: 20,
               shadowOpacity: 0.5}}>
 
-              <Button onPress={() => this.changeBackground()}
-              title="Throw Colors"
-              color= 'black' />
+            <Button onPress={() => this.changeBackground()}
+            title="Holi Time"
+            color= 'white'
+            />
           </View>
         </View>
         <View style={styles.view5}>
-              <TextInput
-              style={styles.inputContainer}
-              value={this.state.text}
-              onChangeText={(text) => this.setState({text})}
-              onSubmitEditing={this.handleSubmit}
-              />
         </View>
-        <View style={styles.view6}>
-            <Text>
-              hello
-            </Text>
-        </View>
+
       </ScrollView>
     );
   }
@@ -118,21 +83,10 @@ const styles = StyleSheet.create({
     height: 160,
     shadowOpacity: 0.5
   },
-
-  inputContainer: {
-    height: 50,
-    fontSize: 25, 
-    borderColor: '#EFB599', 
-    borderRadius: 10,
-    borderWidth: 1, 
-    alignItems: 'center',
-    color: 'white',
-  },
-
   view1: {
     flex: 1,
     backgroundColor: 'wheat',
-    height: 30
+    height: 35
   },
  
   view2: {
@@ -144,28 +98,14 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     height: 200,
-    //backgroundColor: '#6A5D7B',
-    backgroundColor: 'black',
-
+    backgroundColor: '#374F5B',
     justifyContent: 'space-between',
     alignItems: 'center',
     padding: 20
   },
-  view4: {
-    height: 400,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
   view5: {
     flex: 1,
-    backgroundColor: '#E37982',
-    height: 200,
-    padding: 60,
-    justifyContent: 'center',
-    alignContent: 'center'
+    backgroundColor: 'wheat',
+    height: 200
   },
-  view6: {
-    height: 600,
-    backgroundColor: 'lightpink'
-  }
 });
